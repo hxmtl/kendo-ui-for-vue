7adbcda58fc6b7c9d406d7f7eed3343d79dda7a5
Vladimir Milev <vmilev@outlook.com>
2018-03-21 14:43:52 +0200

    demo fix

--- demos/live-code/dashboard/src/components/Dashboard.vue
+++ demos/live-code/dashboard/src/components/Dashboard.vue
@@ -3,7 +3,7 @@
   <h1>Dashboard</h1>
   <kendo-datasource ref="sharedDataSource" :data="localData" :schema-model-fields="schemaModelFields">
   </kendo-datasource>
-  <kendo-grid :data-source-ref="'sharedDataSource'" :editable="true" :sortable="true" :filterable="true">
+  <kendo-grid :data-source-ref="'sharedDataSource'" :sortable="true" :filterable="true">
         <kendo-grid-column field="name" title="Employee"></kendo-grid-column>
         <kendo-grid-column field="sales" title="Sales" type="number" :format="'{0:c}'"></kendo-grid-column>
         <kendo-grid-column field="birthdate" title="Birthdate" type="date" :format="'{0: yyyy-MM-dd}'"></kendo-grid-column>
